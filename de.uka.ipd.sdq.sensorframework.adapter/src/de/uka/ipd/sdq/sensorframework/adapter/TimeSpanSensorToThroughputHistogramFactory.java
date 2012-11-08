e194f4cbeba0463e52c2c4671446baff23175cf1
ChristophRathfelder <christoph.rathfelder@Hahn-Schickard.de>
2012-11-08 14:44:15 +0000

    PALLADIO-199: Removed Throuput entry
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SensorFramework/trunk@16961 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.sensorframework.adapter/src/de/uka/ipd/sdq/sensorframework/adapter/TimeSpanSensorToThroughputHistogramFactory.java
+++ de.uka.ipd.sdq.sensorframework.adapter/src/de/uka/ipd/sdq/sensorframework/adapter/TimeSpanSensorToThroughputHistogramFactory.java
@@ -7,11 +7,11 @@ import de.uka.ipd.sdq.sensorframework.entities.TimeSpanSensor;
 public class TimeSpanSensorToThroughputHistogramFactory implements IAdapterFactory {
 
 	public boolean canAdapt(Object adaptee, Class<?> targetClass) {
-		if (adaptee instanceof SensorAndMeasurements){
-			SensorAndMeasurements sam = (SensorAndMeasurements) adaptee;
-			if (sam.getSensor() instanceof TimeSpanSensor && Histogram.class == targetClass)
-				return true;
-		}
+//		if (adaptee instanceof SensorAndMeasurements){
+//			SensorAndMeasurements sam = (SensorAndMeasurements) adaptee;
+//			if (sam.getSensor() instanceof TimeSpanSensor && Histogram.class == targetClass)
+//				return true;
+//		}
 		return false;
 	}
 	
