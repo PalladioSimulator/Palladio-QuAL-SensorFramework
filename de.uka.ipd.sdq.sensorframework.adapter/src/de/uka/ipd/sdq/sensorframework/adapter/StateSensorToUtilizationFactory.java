0e4c8fc48a2966439148cedb17cd595f15cbc37d
groenda <groenda@fzi.de>
2013-09-25 13:59:13 +0000

    Implementation for feature described in #PALLADIO-263 Report for resource utilization over time intervals. Includes tests in separate plug-in.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SensorFramework/trunk@19376 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.sensorframework.adapter/src/de/uka/ipd/sdq/sensorframework/adapter/StateSensorToUtilizationFactory.java
@@ -0,0 +1,33 @@
+package de.uka.ipd.sdq.sensorframework.adapter;
+
+import de.uka.ipd.sdq.codegen.simudatavisualisation.datatypes.Utilization;
+import de.uka.ipd.sdq.sensorframework.entities.SensorAndMeasurements;
+import de.uka.ipd.sdq.sensorframework.entities.StateSensor;
+
+public class StateSensorToUtilizationFactory implements IAdapterFactory {
+
+	public boolean canAdapt(Object adaptee, Class<?> targetClass) {
+		if (adaptee instanceof SensorAndMeasurements){
+			SensorAndMeasurements sam = (SensorAndMeasurements) adaptee;
+			if (sam.getSensor() instanceof StateSensor && Utilization.class == targetClass)
+				return true;
+		}
+		return false;
+	}
+	
+	public DataAdapter getAdapter(Object adaptee) {
+		return new StateToUtilizationAdapter((SensorAndMeasurements) adaptee);
+	}
+
+	public String getMetricLabel() {
+		return "Utilization";
+	}
+
+	public String getAdapterFactoryID() {
+		return "StateSensorToUtilizationFactory";
+	}
+	
+	public boolean createsAdaptersFor(Class<?> targetClass) {
+		return targetClass.isAssignableFrom(Utilization.class);
+	}	
+}
